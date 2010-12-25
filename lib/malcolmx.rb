e39656f6724386f28d1722ca1fc75aa0662d3206
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-12-25 20:45:33 +0530

    added ability to search multiple domains in one go

--- lib/malcolmx.rb
+++ lib/malcolmx.rb
@@ -3,8 +3,29 @@ module MalcolmX
   require 'httparty'
   require 'crack/json'
 
-  def self.find(domain_name)
-    response = HTTParty.get("http://instantdomainsearch.com/services/quick/?name=vabhub")
+  def self.find(domain_names = [], &block)
+    if domain_names.empty?
+      show_instructions 
+      return
+    end
+
+    domain_names.collect do |domain|
+      response = lookup(domain)
+      response.delete("name")
+      parsed_response = response.collect do |tld, status|
+        {
+          :domain => "#{domain}.#{tld}",
+          :available => status == "a"
+        }
+      end
+      yield(parsed_response)
+    end
+  end
+
+  private
+
+  def self.lookup(domain)
+    response = HTTParty.get("http://instantdomainsearch.com/services/quick/?name=#{domain}")
     json_response = Crack::JSON.parse(response.body)
   end
 end
